14a22564e3cd40466607484a33f532d852b85454
Mary-G10 <merongeremew@yahoo.com>
2025-10-23 13:18:13 -0500

    fix: update base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/evangadi-menu/",
+  base: "/",
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/evangadi-menu/",
+// });
